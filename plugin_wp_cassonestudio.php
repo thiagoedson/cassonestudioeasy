25069d0470a62a390a8042938e4a8ee11ac884a5
Thiago Edson <thiago.edson@itapema.sc.gov.br>
2020-05-11 14:31:46 -0300

    Plugin de fato

---
+++ plugin_wp_cassonestudio.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Nome do plug-in: CassoneStudio Easy
+ * URI do plug-in: https://cassonestudio.com.br/
+ * URI do projeto: https://github.com/thiagoedson/cassonestudioeasy
+ * Descrição: Este é o primeiro plug-in que eu já criei.
+ * Versão: 1.0
+ * Autor: Thiago Edson Pereira
+ * Autor URI: https://cassonestudio.com.br/
+ **/

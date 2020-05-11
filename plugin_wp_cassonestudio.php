2674d2c23e36584f3d82ae8345af4558c0a7c169
Thiago Edson <thiago.edson@itapema.sc.gov.br>
2020-05-11 14:43:37 -0300

    Adicinado função ao plugin do projeto

--- plugin_wp_cassonestudio.php
+++ plugin_wp_cassonestudio.php
@@ -1,10 +1,20 @@
 <?php
 /**
  * Nome do plug-in: CassoneStudio Easy
- * URI do plug-in: https://cassonestudio.com.br/
+ * URI do plug-in: https://cassonestudio.com.br/cassonestudio-easy/
  * URI do projeto: https://github.com/thiagoedson/cassonestudioeasy
  * Descrição: Este é o primeiro plug-in que eu já criei.
- * Versão: 1.0
+ * Versão: 1.0.0
  * Autor: Thiago Edson Pereira
- * Autor URI: https://cassonestudio.com.br/
+ * Autor URI: https://twitter.com/thiagoedson
  **/
+
+
+function dh_modify_read_more_link() {
+
+	return '<a class="more-link" href="' . get_permalink() . '">Leia mais</a>';
+
+}
+
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+

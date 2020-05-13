25699c6935ad4d610a59b598f180d5d601c57ab8
Thiago Edson <thiago.edson@itapema.sc.gov.br>
2020-05-13 09:23:25 -0300

    Ajustes no formulário

--- plugin_wp_cassonestudio.php
+++ plugin_wp_cassonestudio.php
@@ -10,62 +10,67 @@
  **/
 
 
-function dh_modify_read_more_link() {
+function dh_modify_read_more_link()
+{
 
-	return '<a class="more-link" href="' . get_permalink() . '">Leia mais</a>';
-
-}
-
-function orcamento_whatsapp() {
-
-	try {
-
-		$nome      = $_POST['nome'];
-		$email     = $_POST['email'];
-		$produto   = $_POST['produto'];
-		$msg_whats = "Olá meu nome é *" . $nome . "* desejo um orçamento para o *" . $produto . "* , meu e-mail é *" . $email."*";
-		$msg_whats = str_replace( ' ', '%20', $msg_whats );
-
-		$url_whatsa_app = "http://api.whatsapp.com/send?1=pt_BR&phone=5547996125955&text=$msg_whats";
-
-
-		if(isset($_POST['enviar_orcamento'])){
-			?>
-			<script type="text/javascript">
-				window.location = "<?php echo $url_whatsa_app;?>";
-			</script>
-			<?php
-		}
-
-		?>
-
-		<form action="" method="post">
-			<div class="form-group">
-				<label for="nome">Nome</label>
-				<input type="text" name="nome" id="nome">
-			</div>
-			<div class="form-group">
-				<label for="email">E-mail</label>
-				<input type="text" name="email" id="email">
-			</div>
-			<div class="form-group">
-				<label for="produto">Produto</label>
-				<input type="text" name="produto" id="produto">
-			</div>
-			<div class="form-group">
-				<button type="submit" class="btn btn-success">
-					Pedir orçamento
-				</button>
-			</div>
-		</form>
-
-		<?php
-
-	} catch ( Exception $e ) {
-
-	}
+    return '<a class="more-link" href="' . get_permalink() . '">Leia mais</a>';
 
 }
 
-add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+function orcamento_whatsapp()
+{
+
+    try {
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $produto = $_POST['produto'];
+        $msg_whats = "Olá meu nome é *" . $nome . "* desejo um orçamento para o *" . $produto . "* , meu e-mail é *" . $email . "*";
+        $msg_whats = str_replace(' ', '%20', $msg_whats);
+
+        $url_whatsa_app = "http://api.whatsapp.com/send?1=pt_BR&phone=5547996125955&text=$msg_whats";
+
+
+        if (isset($_POST['enviar_orcamento'])) {
+            ?>
+            <script type="text/javascript">
+                window.location = "<?php echo $url_whatsa_app;?>";
+            </script>
+            <?php
+        }
+
+        /**
+         * Gera o html do formulário
+         **/
+        ?>
+
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" id="nome">
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="text" name="email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="produto">Produto</label>
+                <input type="text" name="produto" id="produto">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success">
+                    Pedir orçamento
+                </button>
+            </div>
+        </form>
+
+        <?php
+
+    } catch (Exception $e) {
+
+    }
+
+}
+
+add_filter('the_content_more_link', 'dh_modify_read_more_link');
 
